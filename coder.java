abba1f65052d8dfa15eb56b4ee2af38583f19e5b
Aubrey da Cunha <adacunha@adacunha-deb6-64.(none)>
2013-01-09 20:44:15 -0500

    Added communication framework

---
+++ coder.java
@@ -0,0 +1,24 @@
+public class coder{
+	// This will convert a 15 bit unsigned input to a 31 bit codeword
+
+	// input   : 15 bits
+	// salt    : 15 bits
+	// modulus : 31 bits
+	// keys    : 31 bits
+	static private final long encodeKey = 0x16E114E9L;		
+	static private final long decodeKey = 0x19C68883L;	
+	static private final long modulus   = 0x77587A95L;
+    static private final int  teamID    = 0xA800;
+    	
+	static public int encode(int input){
+		return (int)((((long)(teamID+Math.random()*0x100)*0x8000L+input)*encodeKey) % modulus);
+	}
+
+	static public int decode(int input){
+		return (int)(((input*decodeKey) % modulus) % 0x8000L);
+	}	
+	
+	static public boolean validate(int input){
+	    return (int)((input*decodeKey)%modulus)/0x1000000==0xA8;
+	}
+}
